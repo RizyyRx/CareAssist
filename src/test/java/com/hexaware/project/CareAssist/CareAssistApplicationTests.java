12167e8eead8ee5496ac6cdf7553acb2ad4530c1
RizyyRx <rizwankendo@gmail.com>
2025-09-03 23:06:21 +0530

    Initial commit

---
+++ src/test/java/com/hexaware/project/CareAssist/CareAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.project.CareAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
